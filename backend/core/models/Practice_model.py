88af7b5035090abaab44e7092800773480b54017
hwangh84 <skbootcamp20@gmail.com>
2026-01-22 08:10:30 +0900

    2026-01-22: [모델 리팩토링] Practice_model.py 추가(임시)

--- backend/core/models/Practice_model.py
+++ backend/core/models/Practice_model.py
@@ -1,15 +1,23 @@
-# 수정일: 2026-01-20
-# 수정내용: 팀원 C (Order 담당) - 주문 관련 모델 정의
+# 수정일: 2026-01-22
+# 수정내용: practice 모델
 
 from django.db import models
 from .base_model import BaseModel
 
 class Practice(models.Model):
     """
-    팀원 C 담당: practice 모델
+    팀원 C 담당: practice 모델(현재는 임시. 추후 변경 가능)
     """
+    class Meta:
+        db_table = 'gym_practice'  # 원하는 테이블 이름
+
     practice_number = models.CharField(max_length=50, unique=True)
+    practice_id = models.DecimalField(max_digits=12, decimal_places=2)
+    practice_name = models.CharField(max_length=50)
+    practice_price = models.DecimalField(max_digits=12, decimal_places=2)
+    practice_description = models.TextField()
+    practice_status = models.CharField(max_length=20, default='PENDING')
     
 
     def __str__(self):
-        return self.order_number
+        return self.practice_id
