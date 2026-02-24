f274fe2a957e83d1febabc7f869d0831e45e63da
hyun2kim <skbootcamp20@gmail.com>
2026-02-24 20:54:55 +0900

    소영님 요구사항 반영
    1, hero section 전당보기 버튼 삭제
    2. job Planner, interview 버튼에 이모티콘 삭제
    3. 각 unit 소영님이 만들어준 이미지로 교체(unit4 Team Battle 추가)
    4. landing section: 반응형으로 변경

--- frontend/src/features/home/LandingView.vue
+++ frontend/src/features/home/LandingView.vue
@@ -30,16 +30,13 @@
             <span>입장하기!</span>
             <ArrowRight class="btn-arrow" />
           </button>
-          <button @click="scrollToLeaderboard" class="btn-social-v2">
-            전당 확인
-          </button>
           <!-- [2026-02-24] Job Planner 버튼: 로그인한 사용자에게만 노출되도록 v-if 추가 및 아이콘 적용 -->
           <button v-if="isLoggedIn" @click="$emit('open-job-planner')" class="btn-job-planner btn-planner-color">
-            <span>📓 Job Planner</span>
+            <span>Job Planner</span>
           </button>
           <!-- [2026-02-24] Interview 버튼: 로그인한 사용자에게만 노출되도록 v-if 추가 및 아이콘 적용 -->
           <button v-if="isLoggedIn" @click="$emit('open-interview')" class="btn-job-planner btn-interview-color">
-            <span>🎤 Interview</span>
+            <span>Interview</span>
           </button>
         </div>
       </div>
@@ -330,10 +327,10 @@ import {
   Home,
   Play,
   Settings,
-  History,
   LogOut,
   Info,
-  Briefcase
+  Briefcase,
+  Swords
 } from 'lucide-vue-next';
 import AvatarFrame from '@/components/AvatarFrame.vue';
 
@@ -356,7 +353,8 @@ export default {
     Trophy,
     ArrowRight,
     Info,
-    Briefcase
+    Briefcase,
+    Swords
   },
   props: {
     isLoggedIn: Boolean,
